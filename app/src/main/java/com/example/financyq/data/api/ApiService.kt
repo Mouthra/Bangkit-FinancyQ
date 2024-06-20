946d7a37c146be64f661ccc10c83779837e74085
M.Fiqry Septiawan <fikrisep130902@gmail.com>
2024-06-20 11:01:29 +0800

    Penambahan implement ml API

--- app/src/main/java/com/example/financyq/data/api/ApiService.kt
+++ app/src/main/java/com/example/financyq/data/api/ApiService.kt
@@ -21,13 +21,17 @@ import com.example.financyq.data.response.TotalResponse
 import com.example.financyq.data.response.UpdateExpenditureResponse
 import com.example.financyq.data.response.UpdateIncomeResponse
 import com.example.financyq.data.response.UsernameResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiService {
@@ -117,8 +121,14 @@ interface ApiService {
         @Path("username") username: String,
     ): Response<UsernameResponse>
 
-    @GET("api/transactions/classify")
-    suspend fun addImage(
-
-    )
+    @Multipart
+    @POST("/api/transactions/pengeluaran")
+    suspend fun postImage(
+        @Part("idUser") idUser: RequestBody,
+        @Part("jumlah") jumlah: RequestBody,
+        @Part("deskripsi") deskripsi: RequestBody,
+        @Part("kategori") kategori: RequestBody,
+        @Part("sumber") sumber: RequestBody,
+        @Part image: MultipartBody.Part
+    ): Response<ResponseBody>
 }
